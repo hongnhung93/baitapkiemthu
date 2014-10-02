4d1a761dc899b942b2c994fd6d6062faebd012be
hongnhung93 <nguyenhongnhung2551993@gmail.com>
2014-10-02 22:54:19 +0700

    Update IsATriangleTest.java

--- BaiToanTamGiac/src/IsATriangleTest.java
+++ BaiToanTamGiac/src/IsATriangleTest.java
@@ -1,39 +1,60 @@
-import static org.junit.Assert.*;
-
+package triagle;
+import junit.framwork.Assert;
 import org.junit.Test;
-
-
-public class IsATriangleTest {
-
-	@Test
-	public void testNotATriangle() {
-		IsATriangle temp = new IsATriangle();
-		String rTest = new String(); 
-		rTest = temp.isATriangle(0,1,1);
-		assertEquals("NotATriangle", rTest);
-	}
-
-	@Test
-	public void testEquilateral() {
-		IsATriangle temp = new IsATriangle();
-		String rTest = new String(); 
-		rTest = temp.isATriangle(3,3,3);
-		assertEquals("Triangle is Equilateral", rTest);
-	}
-
-	@Test
-	public void testScalene() {
-		IsATriangle temp = new IsATriangle();
-		String rTest = new String(); 
-		rTest = temp.isATriangle(3,4,5);
-		assertEquals("Triangle is Scalene", rTest);
-	}
-	
-	@Test
-	public void testIsosceles() {
-		IsATriangle temp = new IsATriangle();
-		String rTest = new String(); 
-		rTest = temp.isATriangle(4,4,5);
-		assertEquals("Triangle is Isosceles", rTest);
-	}
-}
+public class triagle_Test{
+    @Test
+    public void testcase1() throws Exeption{
+        Triagle t = new Triangle(1,2,4);
+        int expected = -1;
+        int actual=t.CheckTriangle();
+        Assert.assertEquals(expected, actual);
+        }
+    @Test
+    public void testcase2() throws Exeption{
+        Triagle t = new Triangle(4,1,2);
+        int expected = -1;
+        int actual=t.CheckTriangle();
+        Assert.assertEquals(expected, actual);
+        }
+    @Test
+    public void testcase3() throws Exeption{
+        Triagle t = new Triangle(1,4,2);
+        int expected = -1;
+        int actual=t.CheckTriangle();
+        Assert.assertEquals(expected, actual);
+        }
+    @Test
+    public void testcase4() throws Exeption{
+        Triagle t = new Triangle(5,5,5);
+        int expected = -1;
+        int actual=t.CheckTriangle();
+        Assert.assertEquals(expected, actual);
+        }
+    @Test
+    public void testcase5() throws Exeption{
+        Triagle t = new Triangle(2,2,3);
+        int expected = -1;
+        int actual=t.CheckTriangle();
+        Assert.assertEquals(expected, actual);
+        }
+    @Test
+    public void testcase6() throws Exeption{
+        Triagle t = new Triangle(2,3,2);
+        int expected = 1;
+        int actual=t.CheckTriangle();
+        Assert.assertEquals(expected, actual);
+        }
+    @Test
+    public void testcase7() throws Exeption{
+        Triagle t = new Triangle(3,2,2);
+        int expected = 1;
+        int actual=t.CheckTriangle();
+        Assert.assertEquals(expected, actual);
+        }
+    @Test
+    public void testcase8() throws Exeption{
+        Triagle t = new Triangle(3,4,5);
+        int expected = 0;
+        int actual=t.CheckTriangle();
+        Assert.assertEquals(expected, actual);
+        }
