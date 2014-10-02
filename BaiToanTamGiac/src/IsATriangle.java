915e2832ed66e246a7b6b746a52536b875c59078
hongnhung93 <nguyenhongnhung2551993@gmail.com>
2014-10-02 22:21:48 +0700

    Update IsATriangle.java

--- BaiToanTamGiac/src/IsATriangle.java
+++ BaiToanTamGiac/src/IsATriangle.java
@@ -1,30 +1,25 @@
 
-public class IsATriangle {
-	// dau vao la 3 canh tam giac
-	// dau ra la Equilateral,  Isosceles, Scalene,  NotATriangle
-	String isATriangle(int a, int b, int c){
-		String result = new String();
-		int triangle = 1;
-		
-		// Is a triangle?
-		if((a < b+ c) && (b < a + c) && (c < a+ b)){
-			triangle = 1;
-		}else{
-			triangle = 0;
-			result = "NotATriangle";
-		}
-		
-		//Determine triangle type
-		if(triangle == 1){
-			if((a == b) && (b==c)){
-				result= "Triangle is Equilateral";
-			}else if((a != b) && (a != c) && (b != c)){
-				result ="Triangle is Scalene";
-			}else{
-				result = "Triangle is Isosceles";
-			}
-		}
-		return result;
-	}
+package triagle;
 
+public class triagle {
+		public int  (int a, int b, int c)
+		{
+			boolean isTriangle=false;
+			if( a<b+c&&b<a+c&&c<a+b)
+			{
+					isTriagle=true;
+			}
+			else if(isTriagle){
+				if(a==b&&b==c)
+				{
+					return 2;//tam giac deu
+				}
+			else if(( a!=b)&&(b!=c)||(a==c)&&(b!=c)) 
+					return 0;//tam giac thuong
+			else
+			
+					return 1;//tam giac can
+			}
+			return 1;//khong phai tam giac
+		}
 }
